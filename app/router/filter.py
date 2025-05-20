736200733eec1b897a664c1723eb7b31665d97dd
Stijn Scheltinga Koopman <stijnskoop@gmail.com>
2025-05-20 11:04:12 +0200

    ready for database deployment

--- app/router/filter.py
+++ app/router/filter.py
@@ -9,15 +9,21 @@ from typing import List
 router = APIRouter(
 	prefix="/filter",
 	tags=["filter"]
-)	
+)
+
 
 @router.get("/location", status_code=status.HTTP_200_OK, response_model=List[UserOut])
 async def filter_by_location(user: user_dependency, db: db_dependency):
 	users = (
 		db.query(User)
-		.filter(user.latitude is not None)
-		.filter(user.longitude is not None)
+		.filter(User.latitude is not None)
+		.filter(User.longitude is not None)
+		.filter(User.id != user.id)
 		.all()
 	)
+	# users_with_distance = [
+	# 	UserOut()
+	# 	for u in users
+	# ]
 
 	return users
