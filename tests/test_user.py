b2291ced9bc3a4f2e43461c9ba9f824de58f7f96
Stijn Scheltinga Koopman <stijnskoop@gmail.com>
2025-06-02 14:24:33 +0200

    Added endpoint for default avatar

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,7 +3,7 @@ from app.models import User
 
 def test_create_user_valid(db_session, client):
 	user_data = {
-		"email": "user@gmail.com",
+		"email": "user4@gmail.com",
 		"password": "Password123!"
 	}
 	response = client.post("/user/create_user", json=user_data)
@@ -25,7 +25,7 @@ def test_create_user_wrong_email(client):
 
 def test_create_user_wrong_password(client):
 	user_data = {
-		"email": "user@gmail.com",
+		"email": "user4@gmail.com",
 		"password": "password123"
 	}
 	response = client.post("/user/create_user", json=user_data)
@@ -35,7 +35,7 @@ def test_current_user_valid_token(client, auth_headers):
 	response = client.get('/user/current', headers=auth_headers)
 	assert response.status_code == status.HTTP_200_OK
 	data = response.json()
-	assert data["email"] == "test@example.com"
+	assert data["email"] == "user@gmail.com"
 
 
 def test_current_user_no_token(client):
