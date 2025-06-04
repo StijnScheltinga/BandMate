2fc6c34e8f0d4a28ac8d1142eed4d4a974470dd9
Stijn Scheltinga Koopman <stijnskoop@gmail.com>
2025-06-04 10:44:10 +0200

    Test integration into pipeline should succeed

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -4,7 +4,7 @@ def test_location_complete_profile(client, user_with_profile, test_users):
 	user_data = user_with_profile
 	response = client.get('/filter/location', headers=user_data['auth_header'])
 	assert response.status_code == status.HTTP_200_OK
-	assert len(response.json()) == 9
+	assert len(response.json()) == 10
 
 def test_location_incomplete_profile(client, user_without_profile, test_users):
 	user_data = user_without_profile
